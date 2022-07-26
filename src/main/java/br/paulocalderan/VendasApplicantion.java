f2e59551c793443968b52e7c60bc148146367fe7
Paulo Henrique M. Calderan <paulo_calderam@hotmail.com>
2022-07-26 17:06:51 -0300

    Implementação do Rest

--- src/main/java/br/paulocalderan/VendasApplicantion.java
+++ src/main/java/br/paulocalderan/VendasApplicantion.java
@@ -1,12 +1,7 @@
 package br.paulocalderan;
 
-import br.paulocalderan.domain.entity.Cliente;
-import br.paulocalderan.domain.repository.ClienteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VendasApplicantion {
