b6baf7664d9e22fe7b005522cfce22596d470a1d
Paulo Henrique M. Calderan <paulo_calderam@hotmail.com>
2022-08-30 17:47:03 -0300

    Implementação do Swagger

--- src/main/java/br/paulocalderan/config/SecurityConfig.java
+++ src/main/java/br/paulocalderan/config/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable() //mode stateLess
                 .authorizeHttpRequests()
+                .antMatchers(AUTH_WHITELIST).permitAll()
                 .antMatchers("/api/clientes/**")
                 .hasAnyRole("USER", "ADMIN")
                 .antMatchers("/api/produtos/**")
@@ -61,4 +62,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
     }
+
+    private static final String[] AUTH_WHITELIST = {
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+    };
 }
---
+++ src/main/resources/migration.db/V01_implementacao.sql
@@ -0,0 +1,46 @@
+CREATE TABLE cliente
+(
+    id   INTEGER,
+    nome VARCHAR(100),
+    cpf  varchar(11),
+    CONSTRAINT pk_cliente PRIMARY KEY (ID)
+);
+
+CREATE TABLE produto
+(
+    id             INTEGER,
+    descricao      VARCHAR(100),
+    preco_unitario NUMERIC(20, 2),
+    CONSTRAINT pk_produto PRIMARY KEY (ID)
+);
+
+CREATE TABLE pedido
+(
+    id          INTEGER,
+    cliente_id  INTEGER REFERENCES CLIENTE (ID),
+    data_pedido TIMESTAMP,
+    status      VARCHAR(20),
+    total       NUMERIC(20, 2),
+    CONSTRAINT pk_cliente PRIMARY KEY (ID)
+);
+
+CREATE TABLE item_pedido
+(
+    id          INTEGER,
+    pedido_id   INTEGER REFERENCES PEDIDO (ID),
+    produto_id  INTEGER REFERENCES PRODUTO (ID),
+    data_pedido TIMESTAMP,
+    quantidade  INTEGER,
+    CONSTRAINT pk_cliente PRIMARY KEY (ID)
+);
+
+CREATE TABLE usuario
+(
+    id    INTEGER,
+    login VARCHAR(255),
+    senha VARCHAR(255),
+    admin BOOLEAN,
+    CONSTRAINT pk_cliente PRIMARY KEY (ID)
+);
+
+
